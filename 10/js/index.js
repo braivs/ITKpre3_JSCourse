4980c77220c9c6ce94e098bcaac541242e1878cb
briws <briws@pm.me>
2021-02-23 17:59:56 +0300

    lesson 10 complete

---
+++ 10/js/index.js
@@ -0,0 +1,24 @@
+var firstNameEl = document.getElementById('first-nameId');
+var lastNameEl = document.getElementById('last-nameId');
+var adressEl = document.getElementById('adressId');
+var cityEl = document.getElementById('cityId');
+var hobbiesEl = document.getElementById('hobbiesId');
+var avatarEl = document.getElementById('avatarId');
+var avatarContainerEl = document.getElementById('avatarContainerId');
+
+firstNameEl.value = "NewName";
+lastNameEl.setAttribute('value', 'NewLastName');
+
+console.log('lastNameEl.className = ' + lastNameEl.className);
+lastNameEl.className = 'default-input error-input';
+lastNameEl.title = "Wrong last name";
+
+avatarEl.src = 'https://sun9-67.userapi.com/s/v1/ig2/Xr-76BtbvzAr_4LoQU93NMBq75bORtMYLflyzy_d9eQ2Z7Sy9rHHSE-Yu-M-maNPpJC32BhHnA46dzvHXXHmIWhR.jpg?size=200x0&quality=96&crop=2,10,560,560&ava=1';
+
+avatarEl.title = 'I will be React JS developer!';
+cityEl.value = 'kiev';
+hobbiesEl.value = "Выходить из круга перерождения";
+
+avatarContainerEl.innerHTML = '<ul id=\'list\'><li>1</li><li>2</li></ul>'
+
+var list = document.getElementById('list');
