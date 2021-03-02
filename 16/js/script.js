2a6229c436e729db87537b5267cf2abcb2e94f02
briws <briws@pm.me>
2021-03-02 22:12:28 +0300

    lesson 16 finished

---
+++ 16/js/script.js
@@ -0,0 +1,50 @@
+var buttonPlus = document.getElementById('buttonPlus');
+var buttonMinus = document.getElementById('buttonMinus');
+var buttonMultiply = document.getElementById('buttonMultiply');
+var buttonDevide = document.getElementById('buttonDevide');
+
+var input1 = document.getElementById('number1');
+var input2 = document.getElementById('number2');
+
+function getNumber1() {
+  return Number(input1.value);
+}
+function getNumber2() {
+  return Number(input2.value);
+}
+function makeOperation(operationCode) {
+  if (operationCode === "+") {
+    var result = getNumber1() + getNumber2();
+  } else if (operationCode === "-") {
+    var result = getNumber1() - getNumber2();
+  } else if (operationCode === "*") {
+    var result = getNumber1() * getNumber2();
+  } else if (operationCode === "/") {
+    var result = getNumber1() / getNumber2();
+  } else {
+    window.alert('operation is unknown');
+  }
+  window.alert(result);
+}
+
+function onButtonPlusClick() {
+  makeOperation('+');
+}
+
+function onButtonMinusClick() {
+  makeOperation('-');
+
+}
+function onButtonMultiplyClick() {
+  makeOperation('*');
+
+}
+function onButtonDevideClick() {
+  makeOperation('/');
+
+}
+
+buttonPlus.addEventListener('click', onButtonPlusClick);
+buttonMinus.addEventListener('click', onButtonMinusClick);
+buttonMultiply.addEventListener('click', onButtonMultiplyClick);
+buttonDevide.addEventListener('click', onButtonDevideClick);
