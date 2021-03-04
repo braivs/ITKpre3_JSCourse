9bf96b9a89059fa1e4ff5590aca8d713418b3972
briws <briws@pm.me>
2021-03-04 15:20:32 +0300

    homework 15-20 3/4

---
+++ 15-20_homework/js/part3.js
@@ -0,0 +1,17 @@
+// нашёл массив кнопок
+var buttons = document.getElementsByTagName('button');
+// нахожу куда выводить input
+var result = document.getElementsByTagName('input')[0];
+
+// определяю по какой кнопке кликнул
+function onButtonClick(eventObject) {
+  var clickedElement = eventObject.currentTarget;
+  var buttonValue = clickedElement.innerHTML;
+  result.value = result.value + buttonValue;
+}
+
+// добавляю обработчики
+for (var i = 0; i < buttons.length; i++) {
+  var button = buttons[i];
+  button.addEventListener('click', onButtonClick);
+}
