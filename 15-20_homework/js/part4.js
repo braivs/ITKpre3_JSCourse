aed62c5f2aa0534f62323718eb0a63930cf9af1c
briws <briws@pm.me>
2021-03-04 21:22:46 +0300

    15-20 homework 4/4 progress

--- 15-20_homework/js/part4.js
+++ 15-20_homework/js/part4.js
@@ -4,6 +4,7 @@ var buttons = document.getElementsByTagName('button');
 var numberButtons = []; // это массив для чисел
 var operationButtons = []; // это массив для операторов
 
+// вывод результата
 var result = document.getElementsByTagName('input')[0];
 
 // отделяю числа и операторы
@@ -20,20 +21,30 @@ var number2; // переменная для сохранения второго 
 var operation; // переменная для сохранения значения операции на будущее
 
 // листенер для цифровых кнопок
+// TODO тут надо с комментариями описать логику
 function numberButtonClickListener(e) {
   var numberButton = e.currentTarget;
-  if (operation !== undefined) {
-    result.value = "";
-    result.value = result.value + numberButton.innerHTML;
-  }
-  else if (result.value === '0') {
+  if (result.value === '0') {
+    // Начало калькулятора 
+    // Операция не определена && number1 && number2 не запомнены
     result.value = numberButton.innerHTML;
-    number1 = result.value; //сохранил число для расчёта
-  } else if (operation === undefined){
+    number1 = numberButton.innerHTML; //сохранил число для расчёта
+  } else if (operation === undefined) {
     result.value = result.value + numberButton.innerHTML;
-    console.log(result.value);
     number1 = result.value; //сохранил число для расчёта
+  } else if (operation !== undefined && result.value !== '' && number2 !== undefined) {
+    result.value = '';
+  } else if (operation !== undefined && result.value === '') {
+    console.log('тут');
+    result.value = result.value + numberButton.innerHTML;
+    number2 = result.value; //сохранил число для расчёта
   }
+  
+  else if (operation !== undefined && number1 !== undefined && result.value ==='') {
+    result.value = result.value + numberButton.innerHTML;
+  }
+  
+  
 }
 
 // вешаю на все цифровые кнопки обработчик
@@ -41,17 +52,16 @@ for (let i = 0; i < numberButtons.length; i++) {
   numberButtons[i].addEventListener("click", numberButtonClickListener);
 }
 
-
-
-
 // листенер для кнопок операций. Сохранение операции на будущее. Только если было введено первое число
 function operatorButtonClickListener(e) {
   if (number1 !== undefined) {
     operation = e.currentTarget.innerHTML;
-
   }
 }
 
+function resultButtonClickListener(e) {
+  
+}
 
 for (let i = 0; i < operationButtons.length; i++) {
   operationButtons[i].addEventListener("click", operatorButtonClickListener);
