b8b4ae70caa8355d1e215729abe3bfef1b7cc059
briws <briws@pm.me>
2021-03-05 14:50:34 +0300

    15-20 homework 4/4 complete-beta

--- 15-20_homework/js/part4.js
+++ 15-20_homework/js/part4.js
@@ -5,7 +5,9 @@ var numberButtons = []; // это массив для чисел
 var operationButtons = []; // это массив для операторов
 
 // вывод результата
-var result = document.getElementsByTagName('input')[0];
+var displayInputEl = document.getElementsByTagName('input')[0];
+// кнопка равно
+var resultButton = document.getElementById('equal');
 
 // отделяю числа и операторы
 for (var i = 0; i < buttons.length; i++) {
@@ -16,35 +18,19 @@ for (var i = 0; i < buttons.length; i++) {
   }
 }
 
-var number1; // переменная для сохранения первого числа
-var number2; // переменная для сохранения второго числа
+var firstNumber; // переменная для сохранения первого числа
+var secondNumber; // переменная для сохранения второго числа
 var operation; // переменная для сохранения значения операции на будущее
 
 // листенер для цифровых кнопок
-// TODO тут надо с комментариями описать логику
 function numberButtonClickListener(e) {
   var numberButton = e.currentTarget;
-  if (result.value === '0') {
-    // Начало калькулятора 
-    // Операция не определена && number1 && number2 не запомнены
-    result.value = numberButton.innerHTML;
-    number1 = numberButton.innerHTML; //сохранил число для расчёта
-  } else if (operation === undefined) {
-    result.value = result.value + numberButton.innerHTML;
-    number1 = result.value; //сохранил число для расчёта
-  } else if (operation !== undefined && result.value !== '' && number2 !== undefined) {
-    result.value = '';
-  } else if (operation !== undefined && result.value === '') {
-    console.log('тут');
-    result.value = result.value + numberButton.innerHTML;
-    number2 = result.value; //сохранил число для расчёта
+  if (displayInputEl.value === '0') {
+    // если начальное значение = 0, то заменяю его значением нажатой кнопки
+    displayInputEl.value = numberButton.innerHTML;
+  } else {
+    displayInputEl.value = displayInputEl.value + numberButton.innerHTML;
   }
-  
-  else if (operation !== undefined && number1 !== undefined && result.value ==='') {
-    result.value = result.value + numberButton.innerHTML;
-  }
-  
-  
 }
 
 // вешаю на все цифровые кнопки обработчик
@@ -54,16 +40,38 @@ for (let i = 0; i < numberButtons.length; i++) {
 
 // листенер для кнопок операций. Сохранение операции на будущее. Только если было введено первое число
 function operatorButtonClickListener(e) {
-  if (number1 !== undefined) {
-    operation = e.currentTarget.innerHTML;
-  }
+  operation = e.currentTarget.innerHTML;
+  firstNumber = displayInputEl.value; //сохранил число для расчёта
+  displayInputEl.value = '0';
 }
 
 function resultButtonClickListener(e) {
-  
+
 }
 
 for (let i = 0; i < operationButtons.length; i++) {
   operationButtons[i].addEventListener("click", operatorButtonClickListener);
 }
+
+// листенер для кнопки равно
+function resultButtonClickListener() {
+  var secondNumber = Number(displayInputEl.value);
+  firstNumber = Number(firstNumber);
+  var result = 0;
+  if (operation === "+") {
+    var result = sum(firstNumber, secondNumber);
+  } else if (operation === "-") {
+    var result = minus(firstNumber, secondNumber);
+  } else if (operation === "*") {
+    var result = multiply(firstNumber, secondNumber);
+  } else if (operation === "/") {
+    var result = divide(firstNumber, secondNumber);
+  } else {
+    resultBox.value = 'operation is unknown';
+  }
+  displayInputEl.value = result;
+}
+
+// добавил листенер к кнопке равно
+resultButton.addEventListener('click', resultButtonClickListener);
 
