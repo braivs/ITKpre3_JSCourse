5169dced350cd4810efcc8909475e860fca47bd4
briws <briws@pm.me>
2021-03-08 20:00:57 +0300

    lesson 24 part - 14 min

---
+++ 24_this/js/slider.js
@@ -0,0 +1,48 @@
+var slider1 = {
+  imagesUrls: [],
+  curruntImageIndex: 0,
+  showPrevBtn: document.getElementById('show-prev-btn'),
+  showNextBtn: document.getElementById('show-next-btn'),
+  slideImage: document.getElementById('slide-img'),
+
+  start: function () {
+    // subscribe to events
+    this.showPrevBtn.addEventListener('click', function(e) {
+      console.log('hey!');
+      // this.onShowPrevBtnClick
+      slider1.onShowPrevBtnClick
+    } );
+    this.showNextBtn.addEventListener('click', this.onShowNextBtnClick);
+
+    // create images array
+    this.imagesUrls.push('img/slide1.jpg');
+    this.imagesUrls.push('img/slide2.jpg');
+    this.imagesUrls.push('img/slide3.jpg');
+    this.imagesUrls.push('img/slide4.jpg');
+
+    this.slideImage.src = this.imagesUrls[this.curruntImageIndex];
+    // this.showPrevBtn.disabled = true;
+  },
+
+  onShowPrevBtnClick: function(e) {
+    this.curruntImageIndex--;
+    this.slideImage.src = this.imagesUrls[this.curruntImageIndex];
+    this.showNextBtn.disabled = false;
+
+    // disabled next button if need
+    if (this.curruntImageIndex === 0) {
+      showPrevBtn.disabled = true;
+    }
+  },
+
+  onShowNextBtnClick: function(e) {
+    this.curruntImageIndex++;
+    this.slideImage.src = this.imagesUrls[this.curruntImageIndex];
+    this.showPrevBtn.disabled = false;
+
+    // disabled next button if need
+    if (this.curruntImageIndex === (this.imagesUrls.length - 1)) {
+      this.showNextBtn.disabled = true;
+    }
+  }
+}
