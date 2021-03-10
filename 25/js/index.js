f256c5e230db271ae748f15bdb22b79a4ef0b56f
briws <briws@pm.me>
2021-03-10 12:18:33 +0300

    lesson 26 complete

--- 25/js/index.js
+++ 25/js/index.js
@@ -1,5 +1,5 @@
-var slider1 = createNewSlider();
-var slider2 = createNewSlider();
+var slider1 = sliderFactory.createNewSlider();
+var slider2 = sliderFactory.createNewSlider();
 
 slider1.start('briws-slider1');
 slider2.start('briws-slider2');
